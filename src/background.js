94f7036d4f75ddf4ee4ad35c2a423351d4ef51c9
Guido Arnau <guido.arnau@gmail.com>
2015-10-28 22:06:02 +0000

    Delete leftover code

--- src/background.js
+++ src/background.js
@@ -9,7 +9,7 @@ function interceptRequest(request)
 	if (request && request.url) {
 		if (request.type == "main_frame") {
 			if (request.url.indexOf("docs.google.com") == -1) {
-				return {redirectUrl: "https://docs.google.com/viewer?url=" + request.url + "?"};
+				return { redirectUrl: "https://docs.google.com/viewer?url=" + request.url };
 			}
 		}
 	}
